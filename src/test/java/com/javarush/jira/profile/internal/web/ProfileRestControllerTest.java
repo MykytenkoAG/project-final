cf1b7a0e4cd594da1db926eca808fe6395712c14
Oleksandr Mykytenko <46298794+MykytenkoAG@users.noreply.github.com>
2024-02-09 20:25:40 +0200

    v0.24

--- src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/javarush/jira/profile/internal/web/ProfileRestControllerTest.java
@@ -2,10 +2,6 @@ package com.javarush.jira.profile.internal.web;
 
 import com.javarush.jira.AbstractControllerTest;
 import com.javarush.jira.common.util.JsonUtil;
-import com.javarush.jira.login.User;
-import com.javarush.jira.login.UserTo;
-import com.javarush.jira.login.internal.web.UserController;
-import com.javarush.jira.profile.ProfileTo;
 import com.javarush.jira.profile.internal.ProfileMapper;
 import com.javarush.jira.profile.internal.ProfileRepository;
 import com.javarush.jira.profile.internal.model.Profile;
@@ -17,11 +13,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static com.javarush.jira.login.internal.web.UserTestData.*;
 import static com.javarush.jira.profile.internal.web.ProfileRestController.REST_URL;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 class ProfileRestControllerTest extends AbstractControllerTest {
 
@@ -45,42 +38,80 @@ class ProfileRestControllerTest extends AbstractControllerTest {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
     }
 
-//    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-//    @ResponseStatus(HttpStatus.NO_CONTENT)
-//    public void update(@Valid @RequestBody ProfileTo profileTo, @AuthenticationPrincipal AuthUser authUser) {
-//        super.update(profileTo, authUser.id());
-//    }
     @Test
-    @WithUserDetails(value = ADMIN_MAIL)
-    void update() throws Exception {
-        Profile profileBefore = repository.getExisted(ADMIN_ID);
-        ProfileTo profileTo = mapper.toTo(ProfileTestData.getUpdated(ADMIN_ID));
-        profileTo.setMailNotifications(null);
+    @WithUserDetails(value = USER_MAIL)
+    void getNew() throws Exception {
 
-        perform( MockMvcRequestBuilders.put( REST_URL )
-                .contentType( MediaType.APPLICATION_JSON )
-                .content(JsonUtil.writeValue(profileTo))).andExpect(status().isNoContent());
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(ProfileTestData.getNewTo())))
+                .andExpect(status().isNoContent());
 
-//                .content(  )
-                //.andDo( print() )
-//                .andExpect( status().isNoContent() );
+        Profile profileToActual = repository.getExisted(USER_ID);
+        Profile profileToExpected = ProfileTestData.getNew(USER_ID);
 
+        ProfileTestData.PROFILE_MATCHER.assertMatch(profileToActual, profileToExpected);
 
-//        User dbUserBefore = repository.getExistedByEmail(USER_MAIL);
-//        UserTo updatedTo = mapper.toTo(getUpdated());
-//        updatedTo.setPassword(null);
-//        perform(MockMvcRequestBuilders.put(UserController.REST_URL)
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(jsonWithPassword(updatedTo, updatedTo.getPassword())))
-//                .andDo(print())
-//                .andExpect(status().isNoContent());
-//
-//        User dbUserAfter = repository.getExistedByEmail(USER_MAIL);
-//        assertEquals(dbUserBefore.getPassword(), dbUserAfter.getPassword(), "user's password must not be changed");
-//        User updated = getUpdated();
-//        updated.setRoles(dbUserBefore.getRoles());   // user's roles must not be changed
-//        USER_MATCHER.assertMatch(dbUserAfter, updated);
     }
 
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void getUpdated() throws Exception {
+
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(ProfileTestData.getUpdatedTo())))
+                .andExpect(status().isNoContent());
+
+        Profile profileToActual = repository.getExisted(USER_ID);
+        Profile profileToExpected = ProfileTestData.getUpdated(USER_ID);
+
+        ProfileTestData.PROFILE_MATCHER.assertMatch(profileToActual, profileToExpected);
+
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void getInvalidTo() throws Exception {
+
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(ProfileTestData.getInvalidTo())))
+                .andExpect(status().is4xxClientError());
+
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void getWithUnknownNotificationTo() throws Exception {
+
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(ProfileTestData.getWithUnknownNotificationTo())))
+                .andExpect(status().is4xxClientError());
+
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void getWithUnknownContactTo() throws Exception {
+
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(ProfileTestData.getWithUnknownContactTo())))
+                .andExpect(status().is4xxClientError());
+
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void getWithContactHtmlUnsafeTo() throws Exception {
+
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(ProfileTestData.getWithContactHtmlUnsafeTo())))
+                .andExpect(status().is4xxClientError());
+
+    }
 
 }
