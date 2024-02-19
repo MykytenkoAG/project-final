11fd2a5722917d3edb8b32540e0e675e5c8068c6
Oleksandr Mykytenko <46298794+MykytenkoAG@users.noreply.github.com>
2024-02-19 13:56:20 +0200

    v0.24

---
+++ resources/static/js/forms.js
@@ -0,0 +1,31 @@
+function sendData(form, method, url, jwt_token) {
+    const FD = new FormData(form);
+    console.log(FD);
+    const XHR = new XMLHttpRequest();
+    XHR.open(method, url);
+    XHR.onreadystatechange = function() {
+        if (XHR.readyState == XMLHttpRequest.DONE) {
+            window.history.go(-1);
+        }
+    }
+    XHR.setRequestHeader(
+        "Authorization",
+        "Bearer " + jwt_token,
+    );
+    XHR.send(FD);
+}
+
+let forms = document.getElementsByTagName('form');
+
+for(let i = 0; i < forms.length; i++) {
+    // console.log(forms[i]);
+    forms[i].addEventListener('submit', function(event){
+        // console.log('Form submit event detected.');
+        const action = this.getAttribute("action");
+        if(typeof(action)==="undefined"){
+            return;
+        }
+        event.preventDefault();
+        sendData(this, "POST", action, "myJWTToken");
+    }, false);
+}
