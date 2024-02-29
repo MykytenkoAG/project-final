7e06127db1498dfbca9dde202a75c5a17015806d
Oleksandr Mykytenko <46298794+MykytenkoAG@users.noreply.github.com>
2024-02-29 14:13:00 +0200

    v0.28

--- src/test/java/com/javarush/jira/DataSourceH2.java
+++ src/test/java/com/javarush/jira/DataSourceH2.java
@@ -4,11 +4,14 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
 
 import javax.sql.DataSource;
 
 @Configuration
 @Profile("h2")
+@Sql(scripts = {"classpath:db/changelog-test.sql", "classpath:data-h2.sql"}, config = @SqlConfig(encoding = "UTF-8"))
 public class DataSourceH2 {
 
     @Bean
--- src/test/java/com/javarush/jira/DataSourcePG.java
+++ src/test/java/com/javarush/jira/DataSourcePG.java
@@ -4,11 +4,14 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
 
 import javax.sql.DataSource;
 
 @Configuration
 @Profile("test")
+@Sql(scripts = {"classpath:db/changelog-test.sql", "classpath:data-pg.sql"}, config = @SqlConfig(encoding = "UTF-8"))
 public class DataSourcePG {
 
     @Bean
